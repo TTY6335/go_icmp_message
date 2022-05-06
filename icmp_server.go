9f6c7f61e1fe80d17622a13f1d97480e57df1754
ogawa tetsuya <ogwtetsuya@gmail.com>
2022-05-06 21:26:07 +0900

    icmpの送信を関数化

--- icmp_server.go
+++ icmp_server.go
@@ -11,9 +11,49 @@ import (
 	"golang.org/x/net/ipv4"
 )
 
+func send_icmp(dst_ip *net.IPAddr,IcmpPcaket *icmp.PacketConn, host_ip string,send_data string){
+	fmt.Printf("A%T %s %s\n",IcmpPcaket,host_ip,send_data)
+
+//	//Resolve Destination IP DNS
+//	dst_ip, err := net.ResolveIPAddr("ip4", host_ip)
+//	if err != nil {
+//		log.Fatalf("ResolveIPAddr: %v", err)
+//	}
+
+
+	//Make ICMP Message
+	wm := icmp.Message{
+		Type: ipv4.ICMPTypeEcho,
+		Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff,
+			Data: []byte(send_data),
+		},
+	}
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		log.Fatalf("Marshal: %v", err)
+	}
+
+
+	//Send ICMP Packet
+	start := time.Now()
+	_,_ = IcmpPcaket.WriteTo(wb, dst_ip)
+	if err != nil {
+		log.Fatalf("WriteTo: %v", err)
+	}
+
+	IcmpPcaket.SetReadDeadline(time.Now().Add(10 * time.Second))
+	reply := make([]byte, 1500)
+	_, peer_ip, err := IcmpPcaket.ReadFrom(reply)
+	duration := time.Since(start)
+
+	fmt.Println(peer_ip,start,duration,start.Add(duration))
+}
+
 func main() {
 
-	host := "8.8.8.8"
+	host := "10.0.0.1"
 
 	//Start Listen ICPM
 	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
@@ -22,39 +62,15 @@ func main() {
 	}
 	defer c.Close()
 
-	for i := 1; i <= 100; i++ {
+	//Resolve Destination IP DNS
+	dst_ip, err := net.ResolveIPAddr("ip4", host)
+	if err != nil {
+		log.Fatalf("ResolveIPAddr: %v", err)
+	}
 
-		//Resolve Destination IP DNS
-		dst_ip, err := net.ResolveIPAddr("ip4", host)
-		if err != nil {
-			log.Fatalf("ResolveIPAddr: %v", err)
-		}
+	for i := 1; i <= 20; i++ {
 
-		//Make ICMP Message
-		wm := icmp.Message{
-			Type: ipv4.ICMPTypeEcho,
-			Code: 0,
-			Body: &icmp.Echo{
-				ID: os.Getpid() & 0xffff, Seq: i,
-				Data: []byte("8888"),
-			},
-		}
-		wb, err := wm.Marshal(nil)
-		if err != nil {
-			log.Fatalf("Marshal: %v", err)
-		}
-		//Send ICMP Packet
-		start := time.Now()
-		_,_ = c.WriteTo(wb, dst_ip)
-		if err != nil {
-			log.Fatalf("WriteTo: %v", err)
-		}
+		send_icmp(dst_ip,c,host,"8888")
 
-		c.SetReadDeadline(time.Now().Add(10 * time.Second))
-		reply := make([]byte, 1500)
-		_, peer_ip, err := c.ReadFrom(reply)
-		duration := time.Since(start)
-
-		fmt.Println(peer_ip,start,duration,start.Add(duration))
 	}
 }
